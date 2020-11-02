7a6f2d98634a1628e39108d7c3ef833a9a115fad
jonelle-noelani <jonelle.noelani@gmail.com>
2020-11-02 06:13:03 -0800

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,26 @@
-// Code DigitalClicker Component Here
+import React, {Component} from 'react'
+
+export default class DigitalClicker extends Component {
+    constructor( ) {
+        super( )
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    handleClick = () => {
+        let newClick = this.state.timesClicked + 1
+        this.setState({
+            timesClicked: newClick
+        })
+    }
+
+    render() {
+        return(
+            <div>
+            <h3>{this.state.timesClicked}</h3>
+            <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+            </div>
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,48 @@
-// Code YouTubeDebugger Component Here
+import React, {Component} from 'react'
+
+export default class YouTubDebugger extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+        }
+    }
+
+    bitClick = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    }
+
+    resolution = () => {
+        this.setState({
+            settings: {
+            ...this.state.settings, 
+                video: {
+                ...this.state.video,
+                    resolution: '720p'
+                }
+            }
+        })
+    }
+
+    render() {
+        return(
+            <div>
+            <button onClick={this.bitClick} className='bitrate'>BITRATE</button>
+            <button onClick={this.resolution} className='resolution'>RESOLUTION</button>
+            </div>
+        )
+    }
+}
